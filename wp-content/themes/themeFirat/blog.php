f50d4ba205c11889b40a4798dfb3da731cf0b729
ysrael montes de oca <ysrael_1024gm@yahoo.com>
2013-12-24 14:59:02 -0400

    edit

--- wp-content/themes/themeFirat/blog.php
+++ wp-content/themes/themeFirat/blog.php
@@ -17,10 +17,21 @@ get_header(); ?>
 		
 		 <?php while ( have_posts() ) : the_post(); ?>
          
-				<div class="entry-content">
-					<b><?php ///the_field('display_video_in');?>:</b><br><br>
-					<?php //the_title(); ?><br><br>
-					<?php //the_field('video_embed_code'); ?>
+					<div  id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					
+								<h1 class="entry-title"><?php the_title(); ?></h1>
+								<div class="post-info"></div>
+																
+								<!--<div class="entry-content">
+										<a href="" title=""><img width="150" height="150" src="" class="alignleft post-image" alt="" title=""></a><a href=""><p></p> </a>
+								</div>-->							
+								<b><?php ///the_field('display_video_in');?>:</b><br><br>
+								<?php //the_title(); ?><br><br>
+								<?php //the_field('video_embed_code'); ?>
+					
+					
+					
+
 				</div>
 		  <?php endwhile; // end of the loop. ?>	
 		
