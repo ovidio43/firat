5abf0129144fb3871cf84b7ac20e7c04cb664c12
ysrael montes de oca <ysrael_1024gm@yahoo.com>
2013-12-24 14:05:52 -0400

    edit

--- wp-content/themes/themeFirat/archive-tutorial-videos.php
+++ wp-content/themes/themeFirat/archive-tutorial-videos.php
@@ -38,20 +38,35 @@ get_header(); ?>
 					'meta_key'		=> 'video_embed_code',
 					'meta_value'		=> $type
 					));
-					 
-					if($posts)
-					{
+					
+					
+					//$query = new WP_Query( array( 'meta_key' => 'price', 'meta_value' => '22', 'meta_compare' => '<=', 'post_type' => 'product' ) );
+		?>
+		
+		
+					
+		<?php		if($posts){ ?>
+		
+			<div class="entry-content">					
+		
+					<b><?php the_field('display_video_in');?>:</b><br><br>
+					
+					<?php  foreach($posts as $post){
+						
+								setup_postdata($post);
+						?>
+								
+							<?php the_title(); ?><br><br>
+							<?php the_field('video_embed_code'); ?>
 							
-						/*foreach($posts as $post)
-						{							
-						}*/
-					 ?>
-						<span style="color: #333399;"><em>** Students– Please click on Page 2 of myFES Tutorials on how to navigate myFES</em></span>
-		<?php		}
-		?>		  
+						<?php } ?>
+						
+			</div>						
+		<?php }	?>
 		
 		
-		<p class="pages">Pages: </p>
+		<!--<span style="color: #333399;"><em>** Students– Please click on Page 2 of myFES Tutorials on how to navigate myFES</em></span>		
+		<p class="pages">Pages: </p>-->
 		
 		</div>
  </div>
