317d206ca87f69e4f35e8e8c3883613e9a06a0d9
ysrael montes de oca <ysrael_1024gm@yahoo.com>
2013-12-24 11:32:01 -0400

    edit

--- wp-content/themes/themeFirat/archive-tutorial-videos.php
+++ wp-content/themes/themeFirat/archive-tutorial-videos.php
@@ -14,6 +14,16 @@ get_header(); ?>
         
 		<div class="wrap-content">
 		
+		  <?php while ( have_posts() ) : the_post(); ?>
+         
+				<div class="entry-content">
+					<b>Parents:</b><br><br>
+					<?php the_title(); ?><br><br>
+					<?php the_field('video_embed_code'); ?>
+				</div>
+		  <?php endwhile; // end of the loop. ?>
+		
+		
 		</div>
  </div>
 
