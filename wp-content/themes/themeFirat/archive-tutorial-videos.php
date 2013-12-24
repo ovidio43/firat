6a5027e07e95a95fcd713e03a5f5ef364739c163
ysrael montes de oca <ysrael_1024gm@yahoo.com>
2013-12-24 11:23:17 -0400

    edit

---
+++ wp-content/themes/themeFirat/archive-tutorial-videos.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The default template for displaying autocam-media-image
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+ 
+ 
+ <div id="main-content">
+        
+		<div class="wrap-content">
+		
+		</div>
+ </div>
+
+<?php get_footer(); ?>
