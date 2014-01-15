d01e88251f69d2439d359f477fd8dc8b24b49300
aldo adachi <aldoadachi@iconinc.com>
2014-01-15 11:34:35 -0400

    edit

--- wp-content/themes/themeFirat/js/main.js
+++ wp-content/themes/themeFirat/js/main.js
@@ -1,20 +1,23 @@
 jQuery(document).ready(function() {
+    function getHeightWidthElements() {
+        if (jQuery("#sidebar-right").css('top') == "auto") {
+            var w = jQuery(".wrapper").width() - (jQuery(".enabled_twitter_box").width() + jQuery("#sidebar-right").width());
 
-
-
-	function getHeightWidthElements() {
-        if(jQuery("#sidebar-right").css('top')=="auto"){
-            var w =jQuery(".wrapper").width()-(jQuery(".enabled_twitter_box").width()+jQuery("#sidebar-right").width());
-            
-            jQuery("#main-content").css("max-width",w-2);
+            jQuery("#main-content").css("max-width", w - 2);
             jQuery("#main-content").width(w);
-        }else{
-            var w= jQuery(".wrapper").width()-(jQuery(".enabled_twitter_box").width()); 
-            jQuery("#main-content").css("max-width",w-1);
+        } else {
+            var w = jQuery(".wrapper").width() - (jQuery(".enabled_twitter_box").width());
+            jQuery("#main-content").css("max-width", w - 1);
             jQuery("#main-content").width(w);
         }
-
-	}
+        if ($(window).width() <= 320){
+            $('.btn-iframe').show();
+            $('.iframe-form').hide();
+        }else{
+               $('.btn-iframe').hide();
+            $('.iframe-form').show();
+        }
+    }
     getHeightWidthElements();
     jQuery(window).resize(function() {
         getHeightWidthElements();
@@ -22,18 +25,18 @@ jQuery(document).ready(function() {
     jQuery(window).on("orientationchange", function() {
 
     });
-    function activeMenuMobile(selectorMain, selectorHidden){
-        jQuery(selectorMain).bind('click', function(){
-            
-            if(jQuery(this).attr('rel')=='nav-hidden'){
-                jQuery('.active-block').css('display','none');
+    function activeMenuMobile(selectorMain, selectorHidden) {
+        jQuery(selectorMain).bind('click', function() {
+
+            if (jQuery(this).attr('rel') == 'nav-hidden') {
+                jQuery('.active-block').css('display', 'none');
                 jQuery('.active-nav').removeClass('active-nav');
-                jQuery('.mob-nav-item').attr('rel','nav-hidden');
-                jQuery(selectorHidden).css('display','block').addClass('active-block');
-                jQuery(this).attr('rel','nav-show').addClass('active-nav');         
-            }else{
-                jQuery(selectorHidden).css('display','none').removeClass('active-block');
-                jQuery(this).attr('rel','nav-hidden').removeClass('active-nav');  
+                jQuery('.mob-nav-item').attr('rel', 'nav-hidden');
+                jQuery(selectorHidden).css('display', 'block').addClass('active-block');
+                jQuery(this).attr('rel', 'nav-show').addClass('active-nav');
+            } else {
+                jQuery(selectorHidden).css('display', 'none').removeClass('active-block');
+                jQuery(this).attr('rel', 'nav-hidden').removeClass('active-nav');
             }
         });
     }
@@ -41,7 +44,7 @@ jQuery(document).ready(function() {
     activeMenuMobile('.tweets', '#sidebar-left');
     activeMenuMobile('.test-dates-btb', '#sidebar-right');
     activeMenuMobile('.login-btb', '.login');
-   
-       
-    
+
+
+
 });
